67376b7e80ce2b53adb62d59fc5fe7bdc6107a0a
Chase Douglas <chase@immun.io>
2016-05-26 10:37:59 -0700

    Print out contributors by email

--- index.js
+++ index.js
@@ -2,12 +2,22 @@
 
 const exec = require('child_process').exec
 
-exec('git branch -a', (err, stdout, stderr) => {
+exec('git log --pretty=format:%ae', (err, stdout, stderr) => {
   if (err) {
     process.stderr.write(err.message)
     process.exit(err.code)
   }
 
-  if (stdout) process.stdout.write(stdout)
-  if (stderr) process.stderr.write(stderr)
+  var lines = stdout.split('\n')
+  var emails = {}
+  for (i in lines) {
+    var email = lines[i]
+    if (email in emails) {
+      emails[email] = emails[email] + 1
+    } else {
+      emails[email] = 1
+    }
+  }
+
+  console.log(emails)
 })
